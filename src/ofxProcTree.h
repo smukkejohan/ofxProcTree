8d82bc4d409759d2932f53c343055d7b96d5a11b
Johan Bichel Lindegaard <johan@johan.ccexport>
2014-03-27 11:34:14 +0100

    Constructor with branch that takes vec3f.

--- src/ofxProcTree.h
+++ src/ofxProcTree.h
@@ -44,6 +44,9 @@ struct Properties {
     float seed                  = 10;
     float rseed                 = 10;
     
+    float random(float a){
+        return abs(cos(a+a*a));
+    };
     
 };
 
@@ -51,18 +54,15 @@ struct Properties {
 class Branch {
 public:
     
-    Branch * head;
-    Branch * parent;
+    Branch * head   = NULL;
+    Branch * paren  = NULL;
+    Branch * child0 = NULL;
+    Branch * child1 = NULL;
     
-    Branch(Branch * _head, Branch * _parent);
+    Branch(ofVec3f _head);
+    Branch(Branch * _head = NULL, Branch * _parent = NULL);
     ~Branch();
     
-    
-    
-    //child0=null;
-    //child1=null;
-    //parent=null;
-    //head=null;
     int length = 1;
     
     void mirrorBranch(ofVec3f vec, ofVec3f norm, Properties * prop);
@@ -81,8 +81,8 @@ public:
     Properties props;
     
     ProcTree(){
-        props.seed();
-        troot = new Branch(0,props.trunkLength,0);
+        props.setSeed(ofRandom(0.0,10.0));
+        troot = new Branch(ofVec3f(0,props.trunkLength,0));
         
     };
     ~ProcTree(){};
@@ -96,9 +96,52 @@ public:
     
     
     
+    //void flattenArray(input);
     
     
     
     
+};
+
+
+class TreeUtils { // We probably don't need any of this
+    /*
+    var dot=function(v1,v2){
+        return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
+    };
+    var cross=function(v1,v2){
+        return [v1[1]*v2[2]-v1[2]*v2[1],v1[2]*v2[0]-v1[0]*v2[2],v1[0]*v2[1]-v1[1]*v2[0]];
+    };
+    var length=function(v){
+        return Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+    };
+    var normalize=function(v){
+        var l=length(v);
+        return scaleVec(v,1/l);
+    };
+    var scaleVec=function(v,s){
+        return [v[0]*s,v[1]*s,v[2]*s];
+    };
+    var subVec=function(v1,v2){
+        return [v1[0]-v2[0],v1[1]-v2[1],v1[2]-v2[2]];
+    };
+    var addVec=function(v1,v2){
+        return [v1[0]+v2[0],v1[1]+v2[1],v1[2]+v2[2]];
+    };
+    
+    var vecAxisAngle=function(vec,axis,angle){
+        //v cos(T) + (axis x v) * sin(T) + axis*(axis . v)(1-cos(T)
+        var cosr=Math.cos(angle);
+        var sinr=Math.sin(angle);
+        return addVec(addVec(scaleVec(vec,cosr),scaleVec(cross(axis,vec),sinr)),scaleVec(axis,dot(axis,vec)*(1-cosr)));
+    };
+    
+    var scaleInDirection=function(vector,direction,scale){
+        var currentMag=dot(vector,direction);
+        
+        var change=scaleVec(direction,currentMag*scale-currentMag);
+        return addVec(vector,change);
+    };
+    */
     
 };
